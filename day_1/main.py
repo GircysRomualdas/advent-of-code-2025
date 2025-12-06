a61fc402e168f081a7a438628c7a77618f5be2bc
Romas <r2udis@gmail.com>
2025-12-06 23:58:29 +0200

    day 1 part two

--- day_1/main.py
+++ day_1/main.py
@@ -1,5 +1,10 @@
 import os
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+INPUT_DIR = os.path.join(PROJECT_ROOT, "input")
+TEST_DATA_PATH = os.path.join(INPUT_DIR, "test.txt")
+INPUT_DATA_PATH = os.path.join(INPUT_DIR, "input.txt")
+
 
 def read_data(path: str) -> list[str]:
     with open(path, "r") as f:
@@ -7,10 +12,18 @@ def read_data(path: str) -> list[str]:
     return data
 
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-INPUT_DIR = os.path.join(PROJECT_ROOT, "input")
-TEST_DATA_PATH = os.path.join(INPUT_DIR, "test.txt")
-INPUT_DATA_PATH = os.path.join(INPUT_DIR, "input.txt")
+def crossings(start, distance, direction):
+    count = 0
+    for i in range(1, distance + 1):
+        pos = 0
+        if direction == "R":
+            pos = (start + i) % 100
+        elif "L":
+            pos = (start - i) % 100
+
+        if pos == 0:
+            count += 1
+    return count
 
 
 def main():
@@ -24,15 +37,19 @@ def main():
         rotation = item[0]
         distance = int(item[1:])
 
-        if rotation == "L":
-            pointing = (pointing - distance) % 100
-        elif rotation == "R":
+        time_pointed_to_0 = crossings(pointing, distance, rotation)
+
+        if rotation == "R":
             pointing = (pointing + distance) % 100
+        elif "L":
+            pointing = (pointing - distance) % 100
 
-        if pointing == 0:
-            count_0 += 1
+        count_0 += time_pointed_to_0
 
-        print(f"The dial is rotated {item} to point at {pointing}")
+        msg = f"The dial is rotated {item} to point at {pointing}"
+        if time_pointed_to_0 != 0 and pointing != 0:
+            msg += f"; during this rotation, it points at 0 {time_pointed_to_0} time{'s' if time_pointed_to_0 > 1 else ''}."
+        print(msg)
 
     print(f"Number of times the dial points at 0: {count_0}, password: {count_0}")
 
